c23b44bbf7a67dc1c9e829caacb5b100ffd66478
Ivan Russu <2ivanrussu@gmail.com>
2025-06-13 13:10:00 +0500

    added cyrillic suport

--- init.lua
+++ init.lua
@@ -871,6 +871,15 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  {
+    'nativerv/cyrillic.nvim',
+    event = { 'VeryLazy' },
+    config = function()
+      require('cyrillic').setup {
+        no_cyrillic_abbrev = false, -- default
+      }
+    end,
+  },
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
